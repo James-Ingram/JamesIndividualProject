7b6da40173505f5296d5c2154b2e0b25a7877c51
smurfjames101 <smurfjames@gmail.com>
2019-02-09 14:51:43 +0000

    Redeployed - WildFly Up-To-Date

--- target/JamesIndividualProject/resources/scripts/requests.js
+++ target/JamesIndividualProject/resources/scripts/requests.js
@@ -4,12 +4,28 @@ function getAllProducts()
 	let xhr = new XMLHttpRequest();
 	xhr.withcredentials=true;
     xhr.open("GET", "http://localhost:8080/JamesIndividualProject/api/product/getAllProducts");
-    xhr.onload = (res) => {
-    	console.log(res['target']['response']);
-        responseText.innerHTML =res['target']['response'];
+    xhr.onload = () => {
+    	responseText.innerHTML=beautify(JSON.stringify(xhr.responseText))+'<br>';
     }
     xhr.send();
 }
+
+function beautify(input)
+{
+	return input
+	.replace(/\\/g,"  ")
+	.replace("\"[{","<div class=\"ouptut\">")
+	.replace("}]\"")
+	.replace(/\"/g,"")
+	.replace("undefined", "0")
+	.replace(/:/g,": " +'<pre>')
+	.replace(/}/g,"</div>")
+	.replace(/{/g,'<div class=\"ouptut\">')
+	.replace(/\./g, " . ")
+	.replace(/,/g,'</pre>');
+}
+
+
 
 function createProduct()
 {
--- target/classes/import.sql
+++ target/classes/import.sql
@@ -1,1 +1,5 @@
-insert into Product values (DEFAULT,'Standard ring binder, containing descriptive sleeve', 2.80, 3.00, 'Back-To-School','Ring Binder')
+insert into Product values (DEFAULT,'Standard ring binder containing descriptive sleeve', 2.80, 3.00, 'Back-To-School','Ring Binder')
+insert into Product values (DEFAULT,'Long-Lasting whiteboafrd markers 4-pack', 3.60, 5.00, 'Teachers Rebellion','4-Pack Markers')
+insert into Product values (DEFAULT,'Refillable A4 Notepad', 4.5, 6.00, 'Back-To-School','A4 Notepad')
+insert into Product values (DEFAULT,'Classic scientific Calculator', 11.54, 12.00, 'Back-To-School','Scientific Calculator')
+insert into Product values (DEFAULT,'Packet Of 12 Pens', 1.50, 4.00, 'Back-To-School','12 Ball-Point Pens')
