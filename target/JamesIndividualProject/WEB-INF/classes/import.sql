397d3794840b144faac7b72ed4842eef2ffdead0
smurfjames101 <smurfjames@gmail.com>
2019-02-11 00:24:41 +0000

    Created More Data
    More fake data for larger display set

--- target/JamesIndividualProject/WEB-INF/classes/import.sql
+++ target/JamesIndividualProject/WEB-INF/classes/import.sql
@@ -3,3 +3,5 @@ insert into Product values (DEFAULT,'Long-Lasting whiteboafrd markers 4-pack', 3
 insert into Product values (DEFAULT,'Refillable A4 Notepad', 4.5, 6.00, 'Back-To-School','A4 Notepad')
 insert into Product values (DEFAULT,'Classic scientific Calculator', 11.54, 12.00, 'Back-To-School','Scientific Calculator')
 insert into Product values (DEFAULT,'Packet Of 12 Pens', 1.50, 4.00, 'Back-To-School','12 Ball-Point Pens')
+
+insert into stock values (DEFAULT, 16,'2019-02-04','Manchester','2019-02-11','BigStationaryLtd','Salford')
