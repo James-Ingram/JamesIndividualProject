3a89264fd45065d9ac62d0c76039c97e2efb5496
smurfjames101 <smurfjames@gmail.com>
2019-02-16 02:12:40 +0000

    Found The Issue With Single Result Display
    Looping over a single list created an undefined error, solved with If statement

--- src/main/webapp/resources/scripts/visualOutput.js
+++ src/main/webapp/resources/scripts/visualOutput.js
@@ -1,35 +1,61 @@
-function beautifyProduct(object) {
+"use strict";
+function beautifyProduct(object)
+{
 	let outputString = "";
-	for (var i = 0; i < object.length; i++) {
-		outputString += '<div>' + "Product Id:   "
-				+ JSON.stringify(object[i].productId) + '<br>'
-				+ "Product Name: " + JSON.stringify(object[i].productName)
-				+ '<br>' + "Description:  "
-				+ JSON.stringify(object[i].description) + '<br>'
-				+ "Product Line: " + JSON.stringify(object[i].productLine)
-				+ '<br>' + "Price:        " + JSON.stringify(object[i].price)
-				+ '<br>' + "MSRP:         " + JSON.stringify(object[i].mSRP)
-				+ '</div>';
-	}
+	if(object.length>0)
+	{
+	for(var i =0; i<object.length;i++)
+		{
+		outputString += '<div>'+
+		"Product Id:   "+JSON.stringify(object[i].productId)+'<br>'+
+  		"Product Name: "+JSON.stringify(object[i].productName)+'<br>'+
+  		"Description:  "+JSON.stringify(object[i].description)+'<br>'+
+  		"Product Line: "+JSON.stringify(object[i].productLine)+'<br>'+
+  		"Price:        "+JSON.stringify(object[i].price)+'<br>'+
+  		"MSRP:         "+JSON.stringify(object[i].mSRP)+'</div>';
+		}
 	return outputString;
+	}
+	else{
+		outputString = '<div>'+
+		"Product Id:   "+JSON.stringify(object.productId)+'<br>'+
+  		"Product Name: "+JSON.stringify(object.productName)+'<br>'+
+  		"Description:  "+JSON.stringify(object.description)+'<br>'+
+  		"Product Line: "+JSON.stringify(object.productLine)+'<br>'+
+  		"Price:        "+JSON.stringify(object.price)+'<br>'+
+  		"MSRP:         "+JSON.stringify(object.mSRP)+'</div>';
+  		return outputString;
+	}
 }
-function beautifyStock(object) {
+function beautifyStock(object)
+{
 	let outputString = "";
-	for (var i = 0; i < object.length; i++) {
-		outputString += '<div>' + "Stock Id: "
-				+ JSON.stringify(object[i].stockId) + '<br>'
-				+ "Supplier:          " + JSON.stringify(object[i].supplier)
-				+ '<br>' + "Warehouse:         "
-				+ JSON.stringify(object[i].warehouse) + '<br>'
-				+ "Location:          " + JSON.stringify(object[i].location)
-				+ '<br>' + "Amount:            "
-				+ JSON.stringify(object[i].amount) + '<br>'
-				+ "Last Delivery Date:"
-				+ JSON.stringify(object[i].lastDeliveryDate) + '<br>'
-				+ "Next Delivery Date:"
-				+ JSON.stringify(object[i].nextDeliveryDate) + '</div>';
-	}
+	if(object.length>0)
+	{
+	for(var i =0; i<object.length;i++)
+		{
+		outputString += '<div>'+
+		"Stock Id:          "+JSON.stringify(object[i].stockId)+'<br>'+
+  		"Supplier:          "+JSON.stringify(object[i].supplier)+'<br>'+
+  		"Warehouse:         "+JSON.stringify(object[i].warehouse)+'<br>'+
+  		"Location:          "+JSON.stringify(object[i].location)+'<br>'+
+  		"Amount:            "+JSON.stringify(object[i].amount)+'<br>'+
+  		"Last Delivery Date:"+JSON.stringify(object[i].lastDeliveryDate)+'<br>'+
+  		"Next Delivery Date:"+JSON.stringify(object[i].nextDeliveryDate)+'</div>';
+		}
 	return outputString;
+	}
+	else{
+		outputString += '<div>'+
+		"Stock Id:          "+JSON.stringify(object.stockId)+'<br>'+
+		"Supplier:          "+JSON.stringify(object.supplier)+'<br>'+
+		"Warehouse:         "+JSON.stringify(object.warehouse)+'<br>'+
+		"Location:          "+JSON.stringify(object.location)+'<br>'+
+		"Amount:            "+JSON.stringify(object.amount)+'<br>'+
+		"Last Delivery Date:"+JSON.stringify(object.lastDeliveryDate)+'<br>'+
+		"Next Delivery Date:"+JSON.stringify(object.nextDeliveryDate)+'</div>';
+	return outputString;
+	}
 }
 
 function showOptions(tagName) {
